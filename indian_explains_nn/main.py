1f176e817882ad872241e8ccaeec13a1c686f4d6
goyalpramod <goyalpramod1729@gmail.com>
2025-09-26 11:39:01 -0400

    add test code and update config

--- indian_explains_nn/main.py
+++ indian_explains_nn/main.py
@@ -1,8 +1,22 @@
 from manim import *
 
-
-class CreateCircle(Scene):
+class TestConfig(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screens
+        # This text will now automatically use "Virgil" because
+        # it is set as the default in your manim.cfg
+        test_text = Text("Success! This is in virgil!", color=BLACK, font="Virgil 3 YOFF")
+
+        test_latex = MathTex(r"f(x) = x^2 + 2x + 1", color=BLACK, )
+
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+
+        # Group and arrange all objects vertically
+        group = VGroup(test_text, test_latex, circle).arrange(DOWN, buff=0.8)
+
+        # Animate the objects
+        self.play(Write(test_text))
+        self.play(Write(test_latex))
+        self.play(Create(circle))
+        self.wait(2)
+
